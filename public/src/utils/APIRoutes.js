cabaedf827e03ed5dd17e864a07e6cb8d57a1d5d
david-dooga <doogadavid3@gmail.com>
2026-01-09 17:22:12 +0100

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,6 +1,7 @@
 export const host = "http://34.205.146.14:5000"; 
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
+export const logoutRoute = `${host}/api/auth/logout`;
 export const setAvatarRoute = `${host}/api/auth/setavatar`;
 export const allUsersRoute = `${host}/api/auth/allusers`;
 export const sendMessageRoute = `${host}/api/messages/addmsg`;
